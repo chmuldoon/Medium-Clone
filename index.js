41bb18c2263388dab6e27533799aad6dd68e84bd
Charles Muldoon <charm3728@gmail.com>
2020-04-10 01:03:44 -0700

    added graphql and schemas

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const User = require("./models/user");
+const schema = require("./schema/schema");
+const expressGraphQL = require("express-graphql");
 
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true,
+  })
+);
+// app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
+const router = express.Router();
+
+const createNewUser = router.post("/new", (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      // Throw a 400 error if the email address already exists
+      return res
+        .status(400)
+        .json({ email: "A user has already registered with this address" });
+    } else {
+      // Otherwise create a new user
+      console.log(req.body);
+      const newUserObj = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      newUserObj
+        .save()
+        .then((savedUser) => res.json(savedUser))
+        .catch((err) => console.log(err));
+    }
+  });
+});
+
+app.use("/users", createNewUser);
 app.get("/", (req, res) => res.send("Hello World"));
 
 
